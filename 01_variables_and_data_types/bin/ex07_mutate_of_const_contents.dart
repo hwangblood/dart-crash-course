fdc3205d21b5aadfbba377b102afca2bfe2f1586
hwangblood <hwangblood@gmail.com>
2023-02-08 23:22:40 +0800

    Assigning Const and Final

--- 01_variables_and_data_types/bin/ex07_mutate_of_const_contents.dart
+++ 01_variables_and_data_types/bin/ex07_mutate_of_const_contents.dart
@@ -4,7 +4,7 @@ void main(List<String> args) {
   // ! can't assign a value to const variable
   // myList = [4, 5, 6];
 
-  // ! Cannot add to an unmodifiable list
+  // ! also can't mutate contents of const variable
   // myList.add(5);
 
   print(myList);
