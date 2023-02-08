b20f0d21f0b6d768f5eaedd7621cfaf3749c640d
hwangblood <hwangblood@gmail.com>
2023-02-08 23:04:34 +0800

    Mutating of Const Contents

---
+++ 01_variables_and_data_types/bin/ex07_mutate_of_const_contents.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  const myList = [1, 2, 3];
+
+  // ! can't assign a value to const variable
+  // myList = [4, 5, 6];
+
+  // ! Cannot add to an unmodifiable list
+  // myList.add(5);
+
+  print(myList);
+}
