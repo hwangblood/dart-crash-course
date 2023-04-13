de63608658eb11c6c0d5b2df4d552571e53e3a7c
hwangblood <hwangblood@gmail.com>
2023-04-13 23:07:40 +0800

    Naming Conventions

---
+++ 01_variables_and_data_types/bin/ex14_naming_conventions.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  // * The first word should be started with lowercase, and the rest words of
+  // * variable name should be started with upppercase
+  final yourAge = 'hwang';
+  print(yourAge);
+
+  // * this variable should start with 'w', but not 'W', or you can ignore it
+  // ignore: non_constant_identifier_names
+  final Hello = 'Helo';
+  print(Hello);
+  final Wow = 'Wow';
+  print(Wow);
+
+  // * Ignore using a lowerCamelCase variable name
+  // ignore: constant_identifier_names
+  const SomeName = 'hwangblood';
+  print(SomeName);
+}
