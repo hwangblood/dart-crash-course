ca763fb0b76ba5ee5432f92c542f0d703d82686b
hwangblood <hwangblood@gmail.com>
2023-04-13 22:45:36 +0800

    Type Promotion

---
+++ 01_variables_and_data_types/bin/ex12_type_promotion.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  final double hisHeight = 150.55;
+
+  // ! less precise type cannot be assigned a more precise type
+  // final int myHeight = 170.5;
+  final int myHeight = 170;
+
+  // * double has more presice than int
+  final herHeight = myHeight;
+}
