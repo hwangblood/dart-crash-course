def0a370c69f13f39c9fc3d4121a560ecd67be8a
hwangblood <hwangblood@gmail.com>
2023-04-14 21:17:38 +0800

    Unary Postfix Operators

---
+++ 02_operators/bin/ex3_unary_postfix_operators.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  var age = 40;
+
+  /// * print the value, and then substract it
+  print(age--);
+  print(age);
+
+  /// * print the value, and then add it
+  print(age++);
+  print(age);
+}
