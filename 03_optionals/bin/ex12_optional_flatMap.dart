9b28fb3e63a979644d9b6c9537b38817be4a5cf7
hwangblood <hwangblood@gmail.com>
2023-04-21 20:49:00 +0800

    Optional FlatMap

---
+++ 03_optionals/bin/ex12_optional_flatMap.dart
@@ -0,0 +1,28 @@
+void main(List<String> args) {
+  // * Optional FlatMap
+
+  // flatMap works:  1 => '1' , null => null
+
+  String? firstName = 'John';
+  // String? lastName = 'Doe';
+  String? lastName; // this should be null
+
+  final fullName = firstName.flatMap<String>(
+        (String f) => lastName.flatMap(
+          (String l) => '$f $l',
+        ),
+      ) ??
+      'Either first or last name or both are null';
+  print(fullName);
+}
+
+extension FlatMap<T> on T? {
+  R? flatMap<R>(R? Function(T) callback) {
+    final shadow = this;
+    if (shadow == null) {
+      return null;
+    } else {
+      return callback(shadow);
+    }
+  }
+}
