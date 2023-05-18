e1b7dc65735d93a932d94c8aa1c6b6f334eac0ea
hwangblood <hwangblood@gmail.com>
2023-05-18 17:03:09 +0800

    Dynamic Return Types

---
+++ 05_functions/bin/ex3_dynamic_return_types.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  // * Dynamic Return Types
+
+  print(doNothing());
+}
+
+/// dynamic return type is default.
+/// nothing returned, the return value should be null
+dynamic doNothing() {}
