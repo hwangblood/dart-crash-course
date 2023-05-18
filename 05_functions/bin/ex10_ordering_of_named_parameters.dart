7acd36fd60d289c50c6ecd28eef961ab85c36f6a
hwangblood <hwangblood@gmail.com>
2023-05-18 18:40:30 +0800

    Ordering of Named Parameters

---
+++ 05_functions/bin/ex10_ordering_of_named_parameters.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  // * Ordering of Named Parameters
+
+  describePerson();
+  describePerson(age: 20);
+  describePerson(name: 'Foo');
+  describePerson(name: 'Foo', age: 20);
+  describePerson(age: 20, name: 'Foo');
+}
+
+void describePerson({
+  String? name,
+  int? age,
+}) {
+  print('Hello $name, you are $age years old.');
+}
