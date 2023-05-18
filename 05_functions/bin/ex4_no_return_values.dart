c6c432d685fac8b9bbadbe5d3b630697d20d5606
hwangblood <hwangblood@gmail.com>
2023-05-18 18:13:38 +0800

    No Return Values

---
+++ 05_functions/bin/ex4_no_return_values.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  // * No Return Values
+
+  // you would never invoke the function and assign
+  // the result to a variable
+  final void value = greet(); // ! must not do this!
+}
+
+void greet() {
+  print('Greetings!');
+}
