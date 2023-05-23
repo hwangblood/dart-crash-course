4d07aebf7551856004e26fef9953c5272fd93691
hwangblood <hwangblood@gmail.com>
2023-05-23 13:36:18 +0800

    Mapping Lists to Iterables

--- 07_collections/bin/ex6_map_lists_to_iterables.dart
+++ 07_collections/bin/ex6_map_lists_to_iterables.dart
@@ -4,7 +4,7 @@ void main(List<String> args) {
   const names = ['John', 'Jane', 'Jack', 'Jill'];
 
   final upperCaseNames = names.map((name) {
-    print('Map got called');
+    print('map() got called');
     return name.toUpperCase();
   });
 
