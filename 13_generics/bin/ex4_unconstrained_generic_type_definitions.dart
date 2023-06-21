34188c940571f4e301d98f42ca1793c3610410cb
hwangblood <hwangblood@gmail.com>
2023-06-21 16:08:41 +0800

    Unconstrained Generic Type Definitions

---
+++ 13_generics/bin/ex4_unconstrained_generic_type_definitions.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  // * Unconstrained Generic Type Definitions
+
+  const one = KeyValue(1, 2);
+  print(one);
+  const two = KeyValue(1, 2.2); // MapEntry<int, double>
+  print(two);
+  const three = KeyValue(1, 'Foo'); // MapEntry<int, String>
+  print(three);
+  const KeyValue four = KeyValue(1, 2); // MapEntry<dynamic, dynamic>
+  print(four);
+}
+
+typedef KeyValue<K, V> = MapEntry<K, V>;
