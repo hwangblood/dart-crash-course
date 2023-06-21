04065fc4f5c53c8dfba6ad5de32c91ccd445ed54
hwangblood <hwangblood@gmail.com>
2023-06-21 16:26:37 +0800

    Generic Classes with Generic Extensions

---
+++ 13_generics/bin/ex7_generic_classes_with_generic_extensions.dart
@@ -0,0 +1,31 @@
+void main(List<String> args) {
+  // * Generic Classes with Generic Extensions
+
+  const tuple = Tuple(1, 20.2);
+  print(tuple);
+  final swapped = tuple.swap();
+  print(swapped);
+
+  print(tuple.sum);
+  print(swapped.sum);
+}
+
+class Tuple<L, R> {
+  final L left;
+  final R right;
+
+  const Tuple(this.left, this.right);
+
+  @override
+  String toString() => 'Tuple(l: $left, r: $right)';
+}
+
+/// change two values in the tuple
+extension<L, R> on Tuple<L, R> {
+  Tuple<R, L> swap() => Tuple(right, left);
+}
+
+/// get sum of two values in the tuple, when they are both [num] type
+extension<L extends num, R extends num> on Tuple<L, R> {
+  num get sum => left + right;
+}
